45f83119f35c9c7e67728288b64585560ef8acde
Christian Walter <christian.walter@student.tugraz.at>
2017-01-27 20:49:23 +0200

    astar beginning, but bfs should be better

--- search.py
+++ search.py
@@ -279,6 +279,10 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+
+    pacman_state = problem.getStartState()
+    h_cost = heuristic(pacman_state, problem)
+    print "heuristic: ", h_cost
     util.raiseNotDefined()
 
 
